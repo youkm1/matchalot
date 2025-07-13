ad344e557b96980f55527c4e027ab8709b5bd5fb
youkm1 <youkm0806@sookmyung.ac.kr>
2025-07-13 20:47:36 +0900

    fix: R2dbcTranManager를통한 트랜잭션 컨텍스트 관리

--- src/main/java/com/smwu/matchalot/infrastructure/persistence/config/DatabaseConfig.java
+++ src/main/java/com/smwu/matchalot/infrastructure/persistence/config/DatabaseConfig.java
@@ -11,13 +11,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableR2dbcRepositories
+@EnableTransactionManagement
 @EnableR2dbcAuditing
 public class DatabaseConfig  {
 
+    @Bean
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
+    }
     @Value("${spring.r2dbc.url}")
     private String r2dbcUrl;
 
