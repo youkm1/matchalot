a8b3927bf98f0e1d221481f2075c213e3afc0932
youkm1 <youkm0806@sookmyung.ac.kr>
2025-10-22 22:35:05 +0900

    feat: computer_math add

--- src/main/java/com/smwu/matchalot/web/controller/StudyMaterialController.java
+++ src/main/java/com/smwu/matchalot/web/controller/StudyMaterialController.java
@@ -201,7 +201,8 @@ public class StudyMaterialController {
                 Subject.KOREAN_CULTURE_UNDERSTANDING.name(),
                 Subject.CLASSIC_FIELD_STORY.name(),
                 Subject.DIGITAL_PHILOSOPHY.name(),
-                Subject.WESTERN_HISTORY_CULTURE.name()
+                Subject.WESTERN_HISTORY_CULTURE.name(),
+                Subject.COMPUTER_MATH.name()
         );
 
         return Mono.just(ResponseEntity.ok(Map.of(
